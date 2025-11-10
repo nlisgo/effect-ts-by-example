6188852e6899aa02c9b6c7bb9a0026a5cee6218e
nlisgo <n.lisgo@elifesciences.org>
2025-11-10 20:26:07 +0000

    Add reusable schema decode function for LinkHeader parsing

---
+++ src/unary-example.ts
@@ -0,0 +1,50 @@
+import {
+  Array, Effect, Either, pipe, Schema,
+} from 'effect';
+import { type ParseOptions } from 'effect/SchemaAST';
+
+const schemaDecodeUnknown = (
+  options?: ParseOptions,
+) => <A>(schema: Schema.Schema<A>) => (u: unknown) => Schema.decodeUnknown(schema, options)(u);
+
+const items = [
+  {
+    a: 'a',
+    b: 1,
+  },
+  {
+    a: 'b',
+    b: '2',
+  },
+  {
+    a: 3,
+    b: 3,
+  },
+  {
+    a: 'd',
+    b: 4,
+  },
+];
+
+const itemCodec = Schema.Struct({
+  a: Schema.String,
+  b: Schema.Number,
+});
+
+const exampleOne = (unknownItems: Array<unknown>) => pipe(
+  unknownItems,
+  Array.map((item) => Schema.decodeUnknown(itemCodec)(item)),
+  Effect.forEach(Effect.either),
+  Effect.map(Array.filterMap(Either.getRight)),
+);
+
+const exampleTwo = (unknownItems: Array<unknown>) => pipe(
+  unknownItems,
+  Array.map(schemaDecodeUnknown()(itemCodec)),
+  Effect.forEach(Effect.either),
+  Effect.map(Array.filterMap(Either.getRight)),
+);
+
+void Effect.runSync(exampleOne(items).pipe(Effect.map(console.log)));
+
+void Effect.runSync(exampleTwo(items).pipe(Effect.map(console.log)));
